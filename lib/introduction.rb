fdcb519418770fdd8975053c7cdfb1933a9d156b
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-07 23:20:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,6 @@ end
 
 def introduction_with_language(name, language)
   
-  puts "Hi, my name is #{name}, and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
   
 end
